c237fe66c0787c3765a5db85a62e065f0656efdf
Sergey <sergeuses1397gubarev@gmail.com>
2023-01-12 22:07:56 +0300

    feature(chart.js): added chart.js (working)

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,25 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <bar_chart :chartData="testData"></bar_chart>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+<script>
+import bar_chart from '/src/components/bar_chart.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    bar_chart,
+  },
+  data: () => ({
+    testData: {
+      labels: ['Paris', 'Nîmes', 'Toulon', 'Perpignan', 'Autre'],
+      datasets: [
+        {
+          data: [30, 40, 60, 70, 5],
+          backgroundColor: ['#77CEFF', '#0079AF', '#123E6B', '#97B0C4', '#A5C8ED'],
+        }
+      ]
+    }
+  })
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+</script>
---
+++ src/components/bar_chart.vue
@@ -0,0 +1,20 @@
+<template>
+  <BarChart :chartData="data" />
+</template>
+
+<script>
+import { BarChart } from 'vue-chart-3'
+import { Chart, registerables } from "chart.js"
+
+Chart.register(...registerables)
+export default {
+  name: "bar_chart",
+  components: {
+    BarChart
+  },
+  props: {
+    data: Object
+  }
+}
+</script>
+
